38d8213c0cc3b4822008c25cfb5d9fe7e65ecd95
motaouia <med.motaouia@gmail.com>
2023-04-29 18:25:37 +0200

    initial-commit

---
+++ README.md
@@ -0,0 +1,11 @@
+spring-batch-processing-demo
+
+https://spring.io/guides/gs/batch-processing/
+
+What You Will build??
+
+You will build a service that imports data from a CSV spreadsheet, transforms it with custom code, and stores the final results in a database.
+
+What You Need??
+
+JDK17, Maven, Eclipse, 
---
+++ src/main/java/org/medmota/batchprocessing/SpringBatchProcessingDemoApplication.java
@@ -0,0 +1,16 @@
+package org.medmota.batchprocessing;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableBatchProcessing
+public class SpringBatchProcessingDemoApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(SpringBatchProcessingDemoApplication.class, args);
+		System.exit(SpringApplication.exit(SpringApplication.run(SpringBatchProcessingDemoApplication.class, args)));
+	}
+
+}
